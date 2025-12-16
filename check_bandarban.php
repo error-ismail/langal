eb4e545bd11cc5c6c47105b6227dbc3bb1618139
Mofassel Alam Maruf <57296183+10Maruf@users.noreply.github.com>
2025-12-16 14:14:22 +0600

    Maruf dev remove unwanted files (#6)
    * Add pull_shark.txt for badge
    
    * remove unwanted files

--- check_bandarban.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=langol_krishi_sahayak;charset=utf8mb4', 'root', '');
-$r = $pdo->query("SELECT * FROM location WHERE post_office = 'Bandarban Sadar'")->fetch(PDO::FETCH_ASSOC);
-print_r($r);
